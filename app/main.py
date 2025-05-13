ed30785253fdc6dcc0707288924e888ceb62ee00
Kyrylo Mikushyn <mikushin98@gmail.com>
2025-05-13 15:01:24 +0200

    check animals

--- app/main.py
+++ app/main.py
@@ -1,1 +1,35 @@
-# write your code here
+
+class Animal:
+    alive = []
+
+    def __init__(self, name:str, health: int = 100, hidden: bool = False) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        Animal.alive.append(self)
+
+    def __str__(self):
+        return (f"{{Name: {self.name}, "
+                f"Health: {self.health}, "
+                f"Hidden: {self.hidden}}}")
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __del__(self):
+        if self in Animal.alive and self.health <= 0:
+            Animal.alive.remove(self)
+
+
+class Herbivore(Animal):
+
+    def hide(self) -> None:
+            self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    def bite(self, herbivore: Herbivore) -> None:
+        if isinstance(herbivore, Herbivore) and not herbivore.hidden:
+            herbivore.health = max(0, herbivore.health - 50)
+        if herbivore.health <= 0 and herbivore in Animal.alive:
+            Animal.alive.remove(herbivore)
